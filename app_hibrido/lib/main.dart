e23031394d15aed2e137142f06337a94d7bc0c34
Kil Matheus <kil.teixeira@sou.inteli.edu.br>
2024-05-29 15:16:50 -0300

    Reestruturacao e add o .env(rotas url)

--- app_hibrido/lib/main.dart
+++ app_hibrido/lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'cadastrar.dart';
-import 'package:app_hibrido/camera.dart';
 import 'package:app_hibrido/services/notification.dart';
+import 'package:app_hibrido/views/login_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   NotificationService.init();
   runApp(MyApp());
 }
-
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -19,13 +17,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
     NotificationService.allowed();
     super.initState();
   }
-    
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,139 +32,5 @@ class _MyAppState extends State<MyApp> {
       ),
       home: LoginPage(),
     );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final _formKey = GlobalKey<FormState>();
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-  // final _serverUri = TextEditingController();
-  String _message = '';
-
-  Future<void> _login() async {
-    if (_formKey.currentState!.validate()) {
-      String username = _usernameController.text;
-      String password = _passwordController.text;
-
-      
-
-      try {
-        final response = await http.post(
-          Uri.parse('http://172.20.10.7:8000/login'), // IP do host - Máquina WLAN - Nginx
-          //Uri.parse('http://${_serverUri.text}'), // IP do host - Máquina WLAN - Nginx
-          //Uri.parse('http://172.17.0.1:8000/login'),  // IP do host - Máquina - Nginx
-          //Uri.parse('http://172.18.0.5:8000/login'),  // IP do host - Endereço de Rede (Fica trocando)- Nginx
-          //Uri.parse('http://172.18.0.4:5000/login'), // IP do host - Endereço de Rede - Flutter Simulado
-          //Uri.parse('http://172.17.0.1:5000/login'), //Ip do host - Máquina - Docker Compose s/ Nginx - 5000 Flask
-          headers: {'Content-Type': 'application/json'},
-          body: jsonEncode(<String,String>{'email': username, 'password': password}),
-        );
-        if (response.statusCode == 200) {
-          final responseData = jsonDecode(response.body);
-          setState(() {
-            _message = responseData['message'];
-            NotificationService.showNotification('Login', 'Bem vindo $username');
-          });
-        } else {
-          setState(() {
-            _message = 'Erro de servidor: ${response.statusCode}';
-          });
-        }
-      } catch (e) {
-        setState(() {
-          _message = 'Erro de rede: $e';
-        });
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                controller: _usernameController,
-                decoration: InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira seu email';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Senha'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor, insira sua senha';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _login,
-                child: Text('Login'),
-              ),
-              SizedBox(height: 20),
-              Text(
-                _message,
-                style: TextStyle(color: Colors.red),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => CadastrarPage()),
-                  );
-                },
-                child: Text('Cadastrar'),
-              ),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => FilteredImagePage()),
-                    );
-                  },
-                  child: Text('Camera'),
-                ),
-              // ),
-              // TextField(
-              //   controller: _serverUri,
-              //   decoration: InputDecoration(labelText: 'URI do Servidor'),
-              
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    _usernameController.dispose();
-    _passwordController.dispose();
-    super.dispose();
   }
 }
--- app_hibrido/lib/views/login_page.dart
+++ app_hibrido/lib/views/login_page.dart
@@ -1,48 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import 'services/notification.dart';
+import 'package:app_hibrido/controllers/login_controller.dart';
+import 'package:app_hibrido/models/user_model.dart';
+import 'package:app_hibrido/views/register_page.dart';
+import 'package:app_hibrido/views/camera_page.dart';
 
-class CadastrarPage extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   @override
-  _CadastrarPageState createState() => _CadastrarPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _CadastrarPageState extends State<CadastrarPage> {
+class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController();
+  final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
   String _message = '';
+  final LoginController _loginController = LoginController();
 
-  Future<void> _register() async {
+  Future<void> _login() async {
     if (_formKey.currentState!.validate()) {
-      String email = _emailController.text;
-      String password = _passwordController.text;
-
-      try {
-        final response = await http.post(
-          //Uri.parse('http://172.17.0.1:8000/register'), 
-          Uri.parse('http://172.20.10.7:8000/register'), 
-          headers: {'Content-Type': 'application/json'},
-          body: jsonEncode({'email': email, 'password': password}),
-        );
-
-        if (response.statusCode == 200) {
-          final responseData = jsonDecode(response.body);
-          setState(() {
-            _message = responseData['message'];
-            NotificationService.showNotification('Cadastro', 'O $email foi cadastrado com sucesso');
-          });
-        } else {
-          setState(() {
-            _message = 'Erro de servidor: ${response.statusCode}';
-          });
-        }
-      } catch (e) {
-        setState(() {
-          _message = 'Erro de rede: $e';
-        });
-      }
+      UserModel user = UserModel(_usernameController.text, _passwordController.text);
+      String message = await _loginController.login(user);
+      setState(() {
+        _message = message;
+      });
     }
   }
 
@@ -50,7 +30,7 @@ class _CadastrarPageState extends State<CadastrarPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cadastrar'),
+        title: Text('Login'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -60,7 +40,7 @@ class _CadastrarPageState extends State<CadastrarPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               TextFormField(
-                controller: _emailController,
+                controller: _usernameController,
                 decoration: InputDecoration(labelText: 'Email'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -82,13 +62,34 @@ class _CadastrarPageState extends State<CadastrarPage> {
               ),
               SizedBox(height: 20),
               ElevatedButton(
-                onPressed: _register,
-                child: Text('Cadastrar'),
+                onPressed: _login,
+                child: Text('Login'),
               ),
               SizedBox(height: 20),
               Text(
                 _message,
                 style: TextStyle(color: Colors.red),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => RegisterPage()),
+                  );
+                },
+                child: Text('Cadastrar'),
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => CameraPage()),
+                    );
+                  },
+                  child: Text('Camera'),
+                ),
               ),
             ],
           ),
@@ -99,7 +100,7 @@ class _CadastrarPageState extends State<CadastrarPage> {
 
   @override
   void dispose() {
-    _emailController.dispose();
+    _usernameController.dispose();
     _passwordController.dispose();
     super.dispose();
   }
